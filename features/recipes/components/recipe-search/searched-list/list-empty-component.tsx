ed799c1473f9479561661cb593cf8b3568103a9f
Stepan Putsiata <sstepanovich546@gmail.com>
2026-01-28 13:07:33 +0300

    added localization

--- features/recipes/components/recipe-search/searched-list/list-empty-component.tsx
+++ features/recipes/components/recipe-search/searched-list/list-empty-component.tsx
@@ -2,6 +2,7 @@ import LottieView from 'lottie-react-native';
 import { JSX } from 'react';
 import { Text, View } from 'react-native';
 
+import { useLocalization } from '@/features/localization';
 import { animations } from '@/paths';
 
 import { useStyles } from './searched-list.styles';
@@ -14,14 +15,16 @@ export const ListEmptyComponent = ({
   hasSearched = false,
 }: ListEmptyComponentProps): JSX.Element => {
   const styles = useStyles();
+  const { t } = useLocalization('search');
+
   return (
     <View style={styles.emptyContainer}>
       {hasSearched ? (
-        <Text style={styles.emptyText}>Ничего не найдено</Text>
+        <Text style={styles.emptyText}>{t('emptyText')}</Text>
       ) : (
         <>
           <LottieView autoPlay style={styles.animation} source={animations.searchBanner} />
-          <Text style={styles.emptyText}>Попробуй что-нибудь найти</Text>
+          <Text style={styles.emptyText}>{t('tryToSearch')}</Text>
         </>
       )}
     </View>
