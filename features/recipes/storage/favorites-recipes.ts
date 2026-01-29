250510c75296402e6881992651f1573ccbb0863c
Stepan Putsiata <sstepanovich546@gmail.com>
2026-01-29 11:43:50 +0300

    added stoarge

---
+++ features/recipes/storage/favorites-recipes.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { RecipeDetail } from '../types';
+
+const FAVORITES_KEY = 'favorites_recipes';
+
+export const toggleFavoriteRecipe = async (recipe: RecipeDetail): Promise<void> => {
+  const favorites = await getFavoriteRecipes();
+  const isExists = favorites.some((item) => item.idMeal === recipe.idMeal);
+
+  const updatedFavorites = isExists
+    ? favorites.filter((item) => item.idMeal !== recipe.idMeal)
+    : [...favorites, recipe];
+
+  await AsyncStorage.setItem(FAVORITES_KEY, JSON.stringify(updatedFavorites));
+};
+
+export const getFavoriteRecipes = async (): Promise<RecipeDetail[]> => {
+  const data = await AsyncStorage.getItem(FAVORITES_KEY);
+  return data ? JSON.parse(data) : [];
+};
+
+export const isFavorite = async (recipeId: string): Promise<boolean> => {
+  const favorites = await getFavoriteRecipes();
+  return favorites.some((item) => item.idMeal === recipeId);
+};
